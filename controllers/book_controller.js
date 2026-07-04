149da07396c578ef60107f292d0c4a3844f74c16
Win 10 <adeelsultani68@gmail.com>
2026-07-05 00:14:44 +0500

    book module completed

--- controllers/book_controller.js
+++ controllers/book_controller.js
@@ -31,8 +31,52 @@ const allbooks = async (req, res) => {
 
     }
 };
+const bookbyid = async (req, res) => {
+    try {
 
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({
+                success: false,
+                message: "Book not found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: book
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+const deleteBook = async (req, res) => {
+    try {
+
+        const book = await Book.findByIdAndDelete(req.params.id);
+
+        if (!book) {
+            return res.status(404).json({
+                message: "Book not found"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Book deleted successfully"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
 module.exports = {
     createBook,
-    allbooks
+    allBooks,
+    bookbyId,
+    deleteBook
 };
--- routes/bookroutes.js
+++ routes/bookroutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const book=require('../controllers/book_controller')
 
 router.post("/books", book.createBook);
-router.get("/books", book.allbooks);
+router.get("/books", book.allBooks);
+router.get("/books/:id", book.bookbyId);
+router.delete("/books/:id", book.deleteBook);
 
 module.exports=router
